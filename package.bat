b0f6357d90642f4e9762953873b7fbd294c6da4b
Aeva PaleceK <aeva.ntsc@gmail.com>
2022-02-10 22:06:47 -0600

    Updated Packaging Script

--- package.bat
+++ package.bat
@@ -2,7 +2,8 @@ mkdir distrib
 xcopy racket distrib\racket /i
 xcopy models distrib\models /i
 xcopy shaders distrib\shaders /i
+xcopy materials distrib\materials /i
 copy tangerine.exe distrib\tangerine.exe
 copy LICENSE.txt distrib\LICENSE.txt
 copy SDL2.dll distrib\SDL2.dll
-copy libracketcs_d9hn5s.dll distrib\libracketcs_d9hn5s.dll
+copy libracketcs_da32rk.dll distrib\libracketcs_da32rk.dll
