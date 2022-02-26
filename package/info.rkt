15f880a5f0376e8cea84617eeaf404b4c94dda8d
Aeva PaleceK <aeva.ntsc@gmail.com>
2022-02-26 02:58:21 -0600

    Racket Package Paperwork Attempt

---
+++ package/info.rkt
@@ -0,0 +1,3 @@
+#lang info
+(define collection 'multi)
+(define version "0")
---
+++ package/tangerine/info.rkt
@@ -0,0 +1,5 @@
+#lang setup/infotab
+(define name "tangerine")
+(define blurb
+  '("A library for constructing and executing signed distance functions"))
+(define primary-file "main.rkt")
