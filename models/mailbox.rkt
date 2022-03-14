0c223abe4135c2eef02d1266fdd25c6580e18cfe
Aeva PaleceK <aeva.ntsc@gmail.com>
2022-03-14 02:06:44 -0500

    updating copyright dates to 2022

--- models/mailbox.rkt
+++ models/mailbox.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-; Copyright 2021 Aeva Palecek
+; Copyright 2022 Aeva Palecek
 ;
 ; Licensed under the Apache License, Version 2.0 (the "License");
 ; you may not use this file except in compliance with the License.
