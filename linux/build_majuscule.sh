177ff4ec3eb10ccfbd711285a4eac3be81a70ff4
Vivianne Langdon <puttabutta@gmail.com>
2022-06-28 22:11:59 -0700

    initial cmake, gitignore for some generated files

--- linux/build_majuscule.sh
+++ linux/build_majuscule.sh
@@ -2,60 +2,11 @@
 cd `dirname $0`
 cd ..
 
-LUA_DIR=third_party/lua-5.4.4/lua
+cmake -B linux/build/Debug -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  -DCMAKE_C_COMPILER=clang
+cmake -B linux/build/Release -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  -DCMAKE_C_COMPILER=clang
 
-clang++ \
-	-std=c++17 \
-	-fPIC \
-	-U_WIN64 \
-	-Ithird_party/fmt/include \
-	-Ithird_party/imgui \
-	-Ithird_party/imgui/backends \
-	-Ithird_party/racket/include \
-	-Ithird_party/lua-5.4.4 \
-	-Ithird_party \
-	-I/usr/include/racket \
-	-L/usr/lib/racket \
-	`pkg-config --libs --cflags sdl2 gtk+-3.0` \
-	tangerine/*.cpp \
-	third_party/imgui/*.cpp \
-	third_party/imgui/backends/imgui_impl_opengl3.cpp \
-	third_party/imgui/backends/imgui_impl_sdl.cpp \
-	third_party/voxwriter/VoxWriter.cpp \
-	third_party/fmt/src/format.cc \
-	-xc \
-	-std=c17 \
-	third_party/glad/glad.c \
-	$LUA_DIR/lapi.c \
-	$LUA_DIR/ldebug.c \
-	$LUA_DIR/llex.c \
-	$LUA_DIR/lparser.c \
-	$LUA_DIR/lauxlib.c \
-	$LUA_DIR/ldo.c \
-	$LUA_DIR/lmathlib.c \
-	$LUA_DIR/lstate.c \
-	$LUA_DIR/lbaselib.c \
-	$LUA_DIR/ldump.c \
-	$LUA_DIR/lmem.c \
-	$LUA_DIR/lstring.c \
-	$LUA_DIR/lundump.c
-	$LUA_DIR/lcode.c \
-	$LUA_DIR/lfunc.c \
-	$LUA_DIR/loadlib.c \
-	$LUA_DIR/lstrlib.c \
-	$LUA_DIR/lutf8lib.c
-	$LUA_DIR/lcorolib.c \
-	$LUA_DIR/lgc.c \
-	$LUA_DIR/lobject.c \
-	$LUA_DIR/ltable.c \
-	$LUA_DIR/lvm.c
-	$LUA_DIR/lctype.c \
-	$LUA_DIR/linit.c \
-	$LUA_DIR/lopcodes.c \
-	$LUA_DIR/ltablib.c \
-	$LUA_DIR/lzio.c
-	$LUA_DIR/ldblib.c \
-	$LUA_DIR/liolib.c \
-	$LUA_DIR/loslib.c \
-	$LUA_DIR/ltm.c \
-	-o package/tangerine/tangerine.out
+cmake --build linux/build/Release
