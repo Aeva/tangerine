ff4cfa46e52f9db417f17cf222b37e689e5f71d6
Philip McGrath <philip@philipmcgrath.com>
2023-02-14 17:43:19 -0500

    wip: abstract over parsing (not working)
    (cherry picked from commit 3c159c50f446d26e1c10ace7dffd727cfd864c01)

--- linux/cmake/FindRacket.cmake
+++ linux/cmake/FindRacket.cmake
@@ -43,6 +43,7 @@ list(REMOVE_DUPLICATES _toSearch)
 set(Racket_CONFIG_FILE NO)
 
 #if(NOT $CACHE{Racket_CONFIG_DIR})
+# FIXME maybe https://cmake.org/cmake/help/latest/command/cmake_language.html#defer
     set(_toSearch_etc_racket "")
     set(_toSearch_etc "")
     foreach(dir IN LISTS _toSearch)
@@ -61,15 +62,85 @@ set(Racket_CONFIG_FILE NO)
     endforeach()
 #endif()
 
-message(WARNING "File   :\n--------\n${Racket_CONFIG_FILE}\n--------")
-file(READ ${Racket_CONFIG_FILE} _Racket_CONFIG_FILE_content)
-message(WARNING "Content:\n--------\n${_Racket_CONFIG_FILE_content}\n--------")
-string(REGEX MATCH "[(][ \t\r\n]*lib-search-dirs[ \t\r\n]+\.[ \t\r\n]+[^)]+[)]"
-    _matched
-    ${_Racket_CONFIG_FILE_content})
-message(WARNING "ContenT:\n--------\n${_Racket_CONFIG_FILE_content}\n--------")
-message(WARNING "Matched:\n--------\n${_matched}\n--------}")
+function(racket_extract_config_pair entryId)
+    # FRAGILE: breaks if there is a ")" in the string
+    # Also, the result might not `read` as a Racket pair, e.g. for `(k . ())`
+    set(_options "")
+    set(_oneValKeys FROM OUTPUT_VARIABLE)
+    set(_multiValKeys "")
+    cmake_parse_arguments(PARSE_ARGV 2 parse "${_options}" "${_oneValKeys}" "${_multiValKeys}")
+    # parse_UNPARSED_ARGUMENTS
+    # parse_KEYWORDS_MISSING_VALUES
+    string(REGEX MATCH "[(][ \t\r\n]*${entryId}[ \t\r\n]+\.[ \t\r\n]+[^)]+[)]"
+        _found
+        ${parse_FROM}
+    )
+    set(${parse_OUTPUT_VARIABLE} ${_found} PARENT_SCOPE)
+endfunction()
 
+function(racket_extract_last_string)
+    # FRAGILE: breaks if there is a "\"" in the string
+    set(_options "")
+    set(_oneValKeys FROM OUTPUT_VARIABLE)
+    set(_multiValKeys "")
+    cmake_parse_arguments(PARSE_ARGV 2 "parse" "${_options}" "${_oneValKeys}" "${_multiValKeys}")
+    # parse_UNPARSED_ARGUMENTS
+    # parse_KEYWORDS_MISSING_VALUES
+    string(REGEX MATCHALL "\"[^\"]*[\"]"
+        _allRaw
+        ${parse_FROM}
+    )
+    list(POP_BACK _allRaw _lastRaw) # NOTE: mutates _allRaw
+    # remove the opening and closing "\""
+    string(LENGTH ${_lastRaw} _lenRaw)
+    math(EXPR _lenTrimmed "${_lenRaw} - 2")
+    string(SUBSTRING ${_lastRaw} 1 ${_lenTrimmed} _lastTrimmed)
+    set(${parse_OUTPUT_VARIABLE} ${_lastTrimmed} PARENT_SCOPE)
+endfunction()
+
+function(racket_parse_config_entry entryId)
+    # Not correct in general (see comments above), but good enough for hints
+    set(_options "")
+    set(_oneValKeys FROM OUTPUT_VARIABLE)
+    set(_multiValKeys "")
+    cmake_parse_arguments(PARSE_ARGV 2 "parse" "${_options}" "${_oneValKeys}" "${_multiValKeys}")
+    # parse_UNPARSED_ARGUMENTS
+    # parse_KEYWORDS_MISSING_VALUES
+    racket_extract_config_pair(${entryId} FROM ${parse_FROM} OUTPUT_VARIABLE _pair)
+    racket_extract_last_string(FROM ${_pair} OUTPUT_VARIABLE _string)
+    message(WARNING "========\nFound:\n-----\n${entryId}\n${_string}\n========")
+    set(${parse_OUTPUT_VARIABLE} ${_string} PARENT_SCOPE)
+endfunction()
+
+# message(WARNING "File   :\n--------\n${Racket_CONFIG_FILE}\n--------")
+# file(READ ${Racket_CONFIG_FILE} _Racket_CONFIG_FILE_content)
+# message(WARNING "Content:\n--------\n${_Racket_CONFIG_FILE_content}\n--------")
+# string(REGEX MATCH "[(][ \t\r\n]*lib-search-dirs[ \t\r\n]+\.[ \t\r\n]+[^)]+[)]"
+#     _matched
+#     ${_Racket_CONFIG_FILE_content})
+# message(WARNING "ContenT:\n--------\n${_Racket_CONFIG_FILE_content}\n--------")
+# message(WARNING "Matched:\n--------\n${_matched}\n--------}")
+
+racket_parse_config_entry("installation-name"
+    FROM ${_Racket_CONFIG_FILE_content}
+    OUTPUT_VARIABLE _installationName
+)
+racket_parse_config_entry("lib-dir"
+    FROM ${_Racket_CONFIG_FILE_content}
+    OUTPUT_VARIABLE _configLibDir
+)
+racket_parse_config_entry("lib-search-dirs"
+    FROM ${_Racket_CONFIG_FILE_content}
+    OUTPUT_VARIABLE _configLastLibSearchDir
+)
+racket_parse_config_entry("include-dir"
+    FROM ${_Racket_CONFIG_FILE_content}
+    OUTPUT_VARIABLE _configIncludeDir
+)
+racket_parse_config_entry("include-search-dirs"
+    FROM ${_Racket_CONFIG_FILE_content}
+    OUTPUT_VARIABLE _configLastIncludeSearchDir
+)
 
 find_package_handle_standard_args(Racket DEFAULT_MSG Racket_CONFIG_DIR)
 message(FATAL_ERROR "stop here")
