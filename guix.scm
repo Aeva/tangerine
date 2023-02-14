4fc81db223fa03d98bf81957357b83a30e71ba78
Philip McGrath <philip@philipmcgrath.com>
2023-02-13 22:30:48 -0500

    Revert "guix.scm: remove racket stubs for now"
    This reverts commit 167e1a9aac13cd59e97f18b53755c547e1ade484.
    
    (cherry picked from commit 9cc76c10fe58cb159b69520f4d8e0ee5becb5a09)

--- guix.scm
+++ guix.scm
@@ -60,5 +60,74 @@ of Signed Distance Function (SDF) primitive shapes and combining
 operators.  Models are written in Lua, and may be either rendered
 directly or exported to a variety of common 3D model file formats.")
     (license license:asl2.0)))
+#;
+(define-public racket-vec-lib
+  (let ((commit "c3f29bc928d5900971f65965feaae59e1272a3f7")
+        (revision "1")) ; Guix package revision
+    (package
+      (name "racket-vec-lib")
+      (version (git-version "0.0" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/Aeva/vec")
+                      (commit commit)))
+                (sha256
+                 (base32
+                  "0l515l9kx39xqa9skj7pj54gwffypa1d9pri7daj7kdzqs6d8cc8"))
+                (file-name (git-file-name name version))))
+      (build-system racket-build-system)
+      (inputs
+       (list racket-base))
+      (arguments
+       (list #:path "vec-lib"))
+      (home-page "https://pkgs.racket-lang.org/package/vec-lib")
+      (synopsis "Simple vector math library (implementation part)")
+      (description
+       "This package provides a simple Racket library for vector math,
+developed for Tangerine.  For documentation, see the Racket package
+@code{vec}.")
+      (license license:asl2.0))))
+#;
+(define-public racket-tangerine-x86-64-linux
+  (package
+    (name "racket-tangerine-x86-64-linux")
+    (version "0.0")
+    (origin %tangerine-origin)
+    (build-system racket-build-system)
+    (inputs
+     (list racket-base))
+    (arguments
+     (list
+      #:path "package/tangerine-x86_64-linux"
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'patch-info-rkt
+            (lambda args
+              (substitute* "info.rkt"
+                (("[(]define copy-foreign-libs")
+                 "#;(define copy-foreign-libs")))))))
+    (home-page "https://pkgs.racket-lang.org/package/tangerine-x86-64-linux")
+    (synopsis "")
+    (description "")
+    (license license:asl2)))
+#;
+(define-public racket-tangerine
+  (package
+    (name "racket-tangerine")
+    (version "0.0")
+    (origin %tangerine-origin)
+    (build-system racket-build-system)
+    (inputs
+     (list racket-base
+           racket-sandbox-lib
+           racket-tangerine-x86-64-linux
+           racket-vec-lib))
+    (arguments
+     (list #:path "package/tangerine"))
+    (home-page "https://pkgs.racket-lang.org/package/tangerine-x86-64-linux")
+    (synopsis "")
+    (description "")
+    (license license:asl2)))
 
 tangerine
