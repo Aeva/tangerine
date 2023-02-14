8b0b429eed626bd3bef4a973a52ec74840f17a04
Philip McGrath <philip@philipmcgrath.com>
2023-02-14 18:08:58 -0500

    fixup config parsing; more work needed
    If `lib-search-dirs` looks like this:
    
    ```
          (lib-search-dirs
           .
           (#f
            "/gnu/store/amkv7si0qa8s6q84a4qxwlpjs06hkdza-racket-vm-cs-8.7/opt/racket
    -vm/lib"
            "/gnu/store/xmzx5mzv4863yw9kmr2ykndgp37p8if0-sqlite-3.36.0/lib"
            "/gnu/store/plr00nij45964cyy7sfcg5rcsi8hks0h-openssl-1.1.1l/lib"))
    ```
    
    ... we don't want the last one, actually.
    
    Maybe we should just return the list?
    
    (cherry picked from commit 0408d6f1117669526b31ff93b55e8eef2c4264d0)

--- linux/cmake/script.cmake
+++ linux/cmake/script.cmake
@@ -1,66 +1,8 @@
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-# SPDX-FileCopyrightText: © 2023 Philip McGrath <philip@philipmcgrath.com>
+#!/usr/bin/env -S guix shell cmake -- cmake -P
 
-#[=======================================================================[.rst:
+cmake_minimum_required(VERSION 3.24)
 
-FindRacket
-----------
-
-Finds Racket. Currently focused on Racket CS for embedding.
-
-Imported Targets
-^^^^^^^^^^^^^^^^
-
-This module provides the following imported targets, if found:
-
-``Racket::LibRacketCS``
-  The Racket CS library
-
-#]=======================================================================]
-
-# Model: https://cmake.org/cmake/help/latest/manual/cmake-developer.7.html#find-modules
-
-include(FindPackageHandleStandardArgs)
-
-cmake_path(CONVERT "$ENV{Racket_ROOT}" TO_CMAKE_PATH_LIST _env_Racket_ROOT)
-cmake_path(CONVERT "$ENV{CMAKE_PREFIX_PATH}" TO_CMAKE_PATH_LIST _env_CMAKE_PREFIX_PATH)
-set(_toSearch "")
-foreach(dir IN LISTS Racket_ROOT _env_Racket_ROOT CMAKE_PREFIX_PATH _env_CMAKE_PREFIX_PATH)
-    list(APPEND _toSearch ${dir})
-endforeach()
-cmake_path(CONVERT "$ENV{LIB}" TO_CMAKE_PATH_LIST _env_LIB)
-cmake_path(CONVERT "$ENV{INCLUDE}" TO_CMAKE_PATH_LIST _env_INCLUDE)
-cmake_path(CONVERT "$ENV{PATH}" TO_CMAKE_PATH_LIST _env_PATH)
-foreach(dir IN LISTS _env_LIB _env_INCLUDE _env_PATH)
-    cmake_path(GET dir PARENT_PATH _updir)
-    list(APPEND _toSearch ${_updir})
-endforeach()
-foreach(dir IN LISTS CMAKE_INSTALL_PREFIX CMAKE_STAGING_PREFIX CMAKE_SYSTEM_PREFIX_PATH)
-    list(APPEND _toSearch ${dir})
-endforeach()
-list(REMOVE_DUPLICATES _toSearch)
-
-set(Racket_CONFIG_FILE NO)
-
-#if(NOT $CACHE{Racket_CONFIG_DIR})
-# FIXME maybe https://cmake.org/cmake/help/latest/command/cmake_language.html#defer
-    set(_toSearch_etc_racket "")
-    set(_toSearch_etc "")
-    foreach(dir IN LISTS _toSearch)
-        cmake_path(APPEND dir "etc" OUTPUT_VARIABLE _etc)
-        cmake_path(APPEND _etc "racket" OUTPUT_VARIABLE _rkt)
-        list(APPEND _toSearch_etc_racket ${_rkt})
-        list(APPEND _toSearch_etc ${_etc})
-    endforeach()
-    foreach(dir IN LISTS _toSearch_etc_racket _toSearch_etc)
-        cmake_path(APPEND dir "config.rktd" OUTPUT_VARIABLE _file)
-        if(EXISTS ${_file})
-            set(Racket_CONFIG_DIR ${dir} CACHE PATH "Like the --config argument at the command line")
-            set(Racket_CONFIG_FILE ${_file}) # FIXME set this outside to work even if we got Racket_CONFIG_DIR from the user (we may still need to find others
-            break()
-        endif()
-    endforeach()
-#endif()
+set(Racket_CONFIG_FILE "/gnu/store/al90nb7vrjhgbzr71x037c7ny73b03sb-tangerine-racket-layer-8.7/etc/racket/config.rktd")
 
 function(racket_extract_config_pair entryId)
     # FRAGILE: breaks if there is a ")" in the string
@@ -118,15 +60,6 @@ function(racket_parse_config_entry entryId)
     set(${parse_OUTPUT_VARIABLE} ${_string} PARENT_SCOPE)
 endfunction()
 
-# message(WARNING "File   :\n--------\n${Racket_CONFIG_FILE}\n--------")
-# file(READ ${Racket_CONFIG_FILE} _Racket_CONFIG_FILE_content)
-# message(WARNING "Content:\n--------\n${_Racket_CONFIG_FILE_content}\n--------")
-# string(REGEX MATCH "[(][ \t\r\n]*lib-search-dirs[ \t\r\n]+\.[ \t\r\n]+[^)]+[)]"
-#     _matched
-#     ${_Racket_CONFIG_FILE_content})
-# message(WARNING "ContenT:\n--------\n${_Racket_CONFIG_FILE_content}\n--------")
-# message(WARNING "Matched:\n--------\n${_matched}\n--------}")
-
 file(READ ${Racket_CONFIG_FILE} _Racket_CONFIG_FILE_content)
 racket_parse_config_entry("installation-name"
     FROM ${_Racket_CONFIG_FILE_content}
@@ -147,11 +80,4 @@ racket_parse_config_entry("include-dir"
 racket_parse_config_entry("include-search-dirs"
     FROM ${_Racket_CONFIG_FILE_content}
     OUTPUT_VARIABLE _configLastIncludeSearchDir
-)
-
-find_package_handle_standard_args(Racket DEFAULT_MSG Racket_CONFIG_DIR)
-message(FATAL_ERROR "stop here")
-
-mark_as_advanced(
-    Racket_CONFIG_DIR
 )
