387c00a2a8828ed5676f413a055374a4063888d6
Philip McGrath <philip@philipmcgrath.com>
2023-02-23 20:25:48 -0500

    .dir-locals.el: tweak c-basic-offset

--- tangerine/.dir-locals.el
+++ tangerine/.dir-locals.el
@@ -1,4 +1,4 @@
 ((auto-mode-alist . (("\\.h\\'" . c++-mode)))
  (c++-mode . ((c-file-style . "linux")
-              (c-basic-offset . 8)
+              (c-basic-offset . 4)
               (indent-tabs-mode . t))))
