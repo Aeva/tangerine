8d3b0b8f5e7f6d2a3b2e84b3b6f94ec35c0f3f75
Aeva <aeva.ntsc@gmail.com>
2024-04-27 22:56:25 -0500

    added a nix shell thingy

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    cmake
+    pkg-config
+    ncurses5
+    SDL2
+    clang
+  ];
+}
